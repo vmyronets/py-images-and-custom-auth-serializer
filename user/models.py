b32c1c183aa7f565a941e5197d878796f5fb4b06
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-07-10 12:27:19 +0300

    Set default value for password in create_superuser function

--- user/models.py
+++ user/models.py
@@ -29,7 +29,7 @@ class UserManager(DjangoUserManager):
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
 
         extra_fields.setdefault("is_staff", True)
